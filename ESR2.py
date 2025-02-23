9df65126d9b656541c76a5d49ad5acc078c60d73
Gamma-Ray-Busta <ezbrajcic@gmail.com>
2025-02-22 22:28:46 -0500

    Updated ESR2, ESR3

--- ESR2.py
+++ ESR2.py
@@ -3,13 +3,13 @@ import numpy as np
 from wlinreg import *
 
 
-# Calibration data (Voltage -> Magnetic Field)
+# Calibration data
 measurements = 10
 
-V_offset = np.arange(1.00, float(measurements - 3), 1)
+V_offset = np.arange(1.00, float(measurements - 2), 1)
 
-T1 = np.array([10.9, 23.3, 35.4, 47.0, 58.3, 68.9]) * 1e-4  # Tesla
-T2 = np.array([11.0, 22.7, 34.4, 46.0, 57.8, 69.0]) * 1e-4  # Tesla
+T1 = np.array([10.9, 23.3, 35.4, 47.0, 58.3, 68.9, 82.9]) * 1e-4  # Tesla
+T2 = np.array([11.0, 22.7, 34.4, 46.0, 57.8, 69.0, 81.0]) * 1e-4  # Tesla
 
 T_avg = (T1 + T2) / 2
 T_avg_error = np.sqrt((0.2e-4)**2 + (0.2e-4)**2) / 2  # Error propagation
@@ -18,15 +18,15 @@ T_avg_error = np.sqrt((0.2e-4)**2 + (0.2e-4)**2) / 2  # Error propagation
 s_cal, b_cal, s_cal_std, b_cal_std = plot_regression(
     V_offset, T_avg, T_avg_error,
     'Voltage Offset (V)', 'Magnetic Field (T)',
-    'Calibration Plot 1.svg', 'Figure 1: Experiment 1 calibration measurements of voltage offset relationship with magnetic field strengths of helmholtz coils. Offset voltages going in increments of 1.00V from 1.00V to 6.00V.'
+    'Calibration Plot 1.svg', 'Figure 1: Experiment 1 calibration measurements of voltage offset relationship with magnetic field strengths of helmholtz coils. Offset voltages going in increments of 1.00V from 1.00V to 7.00V.'
 )
 
-# Resonance data (Voltage -> Frequency)
+# Resonance data
 V_C_offset = np.array([2.67, 2.70, 3.04, 3.36, 3.65, 3.95, 4.26, 4.55, 4.85, 5.13, 5.44])
 V_C_offset_error = np.array([0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.02, 0.03, 0.02])
 
 f = np.array([90, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180]) * 1e6  # Frequency in Hz
-f_error = np.full(measurements + 1, 1000)  # Assume small frequency errors
+f_error = np.full(measurements + 1, 0.000000001)  # Assume small frequency errors
 
 # Perform resonance regression (Frequency vs Voltage)
 s_frequency, s_frequency_intercept, s_frequency_std, _ = plot_regression(
